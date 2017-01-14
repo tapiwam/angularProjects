424e1160c503e52759960b47b3289f8b65c23ee4
Tapiwa Maruni <tapiwa.maruni@gmail.com>
2017-01-14 15:19:02 -0600

    Start github service

--- githubSearch/app/app.component.ts
+++ githubSearch/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { GithubService } from './services/github.service'
 
 @Component({
   moduleId: module.id,
   selector: 'my-app',
+  providers: [ GithubService ],
   templateUrl: 'app.component.html',
 })
 export class AppComponent  { name = 'Angular'; }
--- githubSearch/app/services/github.service.ts
+++ githubSearch/app/services/github.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
-import 'rxjs/'
+import 'rxjs/add/operator/map'
 
-@Injectable({
+@Injectable()
+export class GithubService {
+    private username:string;
+    private client_id = 'e4a0dbfbbb9e32981c8c';
+    private client_secret = '34f9d4160813d409063de5e6d2dd64065a01b4f1';
 
-})
-export class GithubService { }
+    constructor (private _http: Http){
+        console.log('Github servise ready...');
+        this.username = 'tapiwam';
+    }
+
+    getUser(){
+        return this._http.get('http://api.github.com/users/' + this.username 
+        + '?client_id' + this. client_id + '&client_secret' + this.client_secret) 
+            .map(res => res.json());
+
+    }
+}
