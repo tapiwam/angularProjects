e6e6157dbc3745986d47d70329b99c8ba82d4268
Tapiwa <tapiwa.maruni@gmail.com>
2017-01-29 13:07:22 -0600

    MeanTodo - Basic Todo layout done

--- meanTodo/client/app/app.module.ts
+++ meanTodo/client/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { TodoComponent } from './components/todo/todo.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ 
+                  AppComponent, 
+                  TodoComponent 
+                ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ meanTodo/client/app/components/todo/todo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'todo',
+  templateUrl: 'todo.component.html',
+})
+export class TodoComponent  {  }
