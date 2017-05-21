cf00948e277ae72f7a346332e3dd079ebe93410b
Tapiwa Maruni <tapiwa.maruni@gmail.com>
2017-05-21 13:17:33 -0500

    Addition of basic assignment 2 - binding

---
+++ ng4/first-app/src/app/name/name.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-name',
+  templateUrl: './name.component.html',
+  styleUrls: ['./name.component.css']
+})
+export class NameComponent implements OnInit {
+
+  username="";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onResetUsername(){
+    this.username = "";
+  }
+
+
+}
