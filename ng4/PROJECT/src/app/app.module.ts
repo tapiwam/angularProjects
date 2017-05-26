30235cfe357b38b5622c0635a82ad43da5c0f7dc
Tapiwa Maruni <tapiwa.maruni@gmail.com>
2017-05-26 06:36:22 -0500

    Started project layout

--- ng4/PROJECT/src/app/app.module.ts
+++ ng4/PROJECT/src/app/app.module.ts
@@ -4,10 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipeListComponent } from './recipies/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipies/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipies/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipiesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
   ],
   imports: [
     BrowserModule,
